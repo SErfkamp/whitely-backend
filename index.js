e93e96dc0667d9e61e58fe5ae99ed459f8dbe2c3
Sönke Erfkamp <soenke.erfkamp@tum.de>
2017-06-17 18:20:33 +0200

    add noti endpoint

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ server.listen(3000, '127.0.0.1', function () {
     console.log('Server listening on port 3000..');
 });
 
+var noti = false;
+
+app.post('/api/noti', (req, res) => {
+    noti = true;
+});
+
+
+app.get('api/noti', (req, res) => {
+    if (noti) {
+        res.json({
+            noti: true
+        });
+        noti = false;
+    } else {
+        res.json({
+            noti: false
+        });
+    }
+});
+
 // Expose app
 module.exports = {
     app: app,
